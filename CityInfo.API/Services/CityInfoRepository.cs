095e127efad6b3bd32d21f8160734018c93745a3
Alex <yeswxlli@gmail.com>
2022-06-14 20:56:45 +0200

    Projektdateien hinzufügen.

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,41 @@
+﻿using CityInfo.API.DbContexts;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            return await _context.Cities.OrderBy(c => c.Name).ToListAsync();
+        }
+
+        public async Task<City?> GetCityAsync(int CityId, bool includePOI)
+        {
+            if (includePOI)
+            {
+                return await _context.Cities.Include(c => c.PointsOfInterest)
+                    .Where(c => c.Id == CityId).FirstOrDefaultAsync();
+            }
+            return await _context.Cities.FirstOrDefaultAsync(c => c.Id == CityId);
+        }
+
+        public async Task<IEnumerable<PointOfInterest>> GetPOISAsync(int CityId)
+        {
+            return await _context.PointsOfInterest.Where(c => c.Id == CityId).ToListAsync();
+        }
+
+        public async Task<PointOfInterest?> GetPOIAsync(int CityId, int POIId)
+        {
+            return await _context.PointsOfInterest.Where(c => c.CityId == CityId).FirstOrDefaultAsync(c => c.Id == POIId);
+        }
+    }
+}
---
+++ CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,13 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        Task<IEnumerable<City>> GetCitiesAsync();
+        Task<City?> GetCityAsync(int CityId, bool includePOI);
+        Task<IEnumerable<PointOfInterest>> GetPOISAsync(int CityId);
+        Task<PointOfInterest?> GetPOIAsync(int CityId, int POIId);
+    }
+}
